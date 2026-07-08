7990531065b19bf9208a9fa0af51522ccffa5e38
TrangDataforlife <trangnguyen08112004@gmail.com>
2026-07-08 13:48:26 +0700

    added two staging models, refactored dim_customer, and established materalization strategy.

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,24 +1,17 @@
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+SELECT *
+FROM
+{{
+    ref('stg_jaffleshop_customer')
+    }}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+SELECT *
+FROM
+{{
+    ref('stg_jaffleshop_order')
+    }}
 ),
 
 customer_orders as (
